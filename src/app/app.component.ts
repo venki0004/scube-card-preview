093d94081304b707d6de16b653d0117bcee33582
venki0004 <venkatesh@scube.me>
2021-05-03 20:28:09 +0530

    build with changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tailwing-angular';
+  name:any
+
+  ngOnInit(): void {
+    var canvas: any = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    const text = 'Your Name!'
+
+    ctx.font = "40px Georgia";
+    ctx.textAlign = 'center'
+    ctx.fillStyle = '#cd944f'
+    ctx.fillText(text, canvas.width/2, canvas.height/2)
+
+
+  }
+
+  valueChange(name:any) {
+console.log(name)
+var canvas: any = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+ctx.fillStyle = "#000";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+const text = name
+
+ctx.font = "40px Georgia";
+ctx.textAlign = 'center'
+ctx.fillStyle = '#cd944f'
+ctx.fillText(text, canvas.width/2, canvas.height/2)
+
+  }
 }
